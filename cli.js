e257718437d32fb129cbde63a7fa69544ac76cba
Sindre Sorhus <sindresorhus@gmail.com>
2019-01-10 10:49:00 +0700

    Init

---
+++ cli.js
@@ -0,0 +1,54 @@
+#!/usr/bin/env node
+'use strict';
+const path = require('path');
+const meow = require('meow');
+const terminalImage = require('terminal-image');
+const termImg = require('term-img');
+const chalk = require('chalk');
+const boxen = require('boxen');
+const uniqueRandomArray = require('unique-random-array');
+const dogeSeed = require('doge-seed');
+
+const isIterm = process.env.TERM_PROGRAM === 'iTerm.app';
+const dogeImage = path.join(__dirname, 'doge.png');
+
+const randomColor = uniqueRandomArray([
+	'red',
+	'green',
+	'yellow',
+	'blue',
+	'magenta',
+	'cyan'
+]);
+
+const cli = meow(`
+	Usage
+	  $ doge-seed [bits]
+
+	The first argument is the number of bits to derive a BIP39 mnemonic from.
+	Must be an integer, divisible by 32, in the range 128...256.
+	The default value is 128.
+`);
+
+(async () => {
+	const bits = cli.input[0];
+
+	let seed = bits ? dogeSeed(parseInt(bits, 10)) : dogeSeed();
+	seed = seed.split(' ').map(x => chalk[randomColor()](x)).join(' ');
+
+	const seedBox = boxen(seed, {
+		float: 'center',
+		padding: 1,
+		borderStyle: 'round',
+		borderColor: 'yellow',
+		dimBorder: true
+	});
+
+	if (isIterm) {
+		termImg(dogeImage, {width: '100%'});
+	} else {
+		console.log(await terminalImage.file(dogeImage));
+	}
+
+	console.log(seedBox, '\n\n\n');
+})();
---
+++ readme.md
@@ -0,0 +1,38 @@
+# doge-seed-cli [![Build Status](https://travis-ci.com/doge-seed-cli/sindresorhus.svg?branch=master)](https://travis-ci.com/sindresorhus/doge-seed-cli)
+
+> Generate dank [mnemonic seed phrases](https://en.bitcoinwiki.org/wiki/Mnemonic_phrase) in the terminal
+
+Generates a cryptographically secure mnemonic seed phrase with added dankness. The first four words will be a randomly generated Doge-like sentence. The seed phrases are fully valid checksummed BIP39 seeds. They can be used with any cryptocurrency and can be imported into any BIP39 compliant wallet. [Read more…](https://github.com/lukechilds/doge-seed)
+
+<img src="screenshot.png" width="1175">
+
+
+## Install
+
+```
+$ npm install --global doge-seed-cli
+```
+
+
+## Usage
+
+```
+$ doge-seed --help
+
+  Usage
+    $ doge-seed [bits]
+
+  The first argument is the number of bits to derive a BIP39 mnemonic from.
+  Must be an integer, divisible by 32, in the range 128...256.
+  The default value is 128.
+```
+
+
+## Related
+
+- [doge-seed](https://github.com/lukechilds/doge-seed) - API for this module
+
+
+## License
+
+MIT © [Sindre Sorhus](https://sindresorhus.com)
